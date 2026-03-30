997062f507d4188141892d137d332c607934111f
Hieng Seng Hua <145307596+hs-hua@users.noreply.github.com>
2026-03-30 10:36:31 -0700

    Implement HashMap population in twoSum method
    Add logic to populate HashMap with indices.

--- Easy/001-Two-Sum/solution.java
+++ Easy/001-Two-Sum/solution.java
@@ -1,5 +1,9 @@
 import java.util.HashMap;
 public int[] twoSum(int[] nums, int target){
    HashMap<Integer,Integer> hm = new HashMap<>();
-   
+   int count = 0;
+   for(int num : nums){
+      hm.put(num, count); //inserting integer key and index value
+      count++;
+   }
 }
